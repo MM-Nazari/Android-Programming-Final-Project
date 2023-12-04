3ed9320b865350e61262b63ada0cc084bc378e26
Mohammad Mehdi Nazari <75282167+MMNazari1380@users.noreply.github.com>
2023-12-04 20:22:36 +0330

    Add files via upload

---
+++ login/app/src/test/java/com/mmnazarisapplication/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.mmnazarisapplication.app
+
+import com.mmnazarisapplication.app.appcomponents.base.BaseActivity
+import com.mmnazarisapplication.app.databinding.LayoutProgressDialogBinding
+
+class MainActivity : BaseActivity<LayoutProgressDialogBinding>(R.layout.layout_progress_dialog) {
+
+    override fun onInitialized() {
+
+    }
+
+    override fun setUpClicks() {
+
+    }
+}
